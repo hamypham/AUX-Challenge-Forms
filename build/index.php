88171438f369987267c83499b5014a4aec181601
hamypham <hamypham@me.com>
2014-03-24 16:53:18 -0400

    Update index.php

--- build/index.php
+++ build/index.php
@@ -6,42 +6,8 @@
 </head>
 
 <body>
-
-    <?php
-        // if the form has been submitted, process it - otherwise, just display the form normally
-        if(isset($_POST['send'])){
-            
-            // pull the name out of the submitted for
-            $name = strip_tags($_POST['name']);
-            
-            // pull the email out of the submitted form
-            $emailFrom = strip_tags($_POST['email']);
-            
-            // who you're sending the email to (probably change this)
-            $emailTo = "apprentices@freshtilledsoil.com";
-            $subject = "Submission";
-            
-            // inset information into the body of the email
-            $body = "Name: ".$name."\n";
-            $body .= "Email: ".$emailFrom."\n";
-            
-            // set the email headers
-            $headers = "From: ".$emailFrom."\n";
-            $headers .= "Reply-To:".$emailFrom."\n";	
-            
-            $success = mail($emailTo, $subject, $body, $headers);
-            
-            // this is the message that gets displayed after submission
-            if ($success){
-                echo 'sent';
-            } else {
-                echo 'not sent';
-            }
         
-        } else {
-    ?>
-        
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
             
             <label for="name">Name</label>
             <input type="text" id="name" name="name" minlength="2"/>
@@ -52,10 +18,6 @@
             <button type="submit" name="send">Send!</button>
             
         </form>
-    
-    <?php
-        }
-    ?>
 
 </body>
 </html>
